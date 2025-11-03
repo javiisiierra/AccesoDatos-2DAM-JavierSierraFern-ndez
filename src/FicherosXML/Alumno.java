c0719b5c71703078f202c3ca397a6d53691958e6
diurno <diurno@equipo-00>
2025-11-03 08:34:19 +0100

    3/11/25

---
+++ src/FicherosXML/Alumno.java
@@ -0,0 +1,38 @@
+package FicherosXML;
+
+public class Alumno {
+	
+	private int numExpediente;
+	private String nombre;
+	private double nota;
+	
+
+	public int getNumExpediente() {
+		return numExpediente;
+	}
+
+	public void setNumExpediente(int numExpediente) {
+		this.numExpediente = numExpediente;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getNota() {
+		return nota;
+	}
+
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+
+
+	
+	
+
+}
